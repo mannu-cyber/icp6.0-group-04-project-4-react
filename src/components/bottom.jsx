10036c8dc375a025214adf497bedb651bf29d065
Vikrantpatil19 <pvikrant010@gmail.com>
2024-02-21 18:26:33 +0530

    Add the horizontal button

--- src/components/bottom.jsx
+++ src/components/bottom.jsx
@@ -1,9 +1,10 @@
+import { borderRadius } from '@mui/system';
 import React from 'react'
 
 function Bottom() {
 
     const customStyles = {
-        backgroundColor: 'blue',
+        
         color: 'white',
         padding: '20px',
         borderRadius: '5px',
@@ -11,10 +12,24 @@ function Bottom() {
         textAlign: 'center'
     };
 
+    const buttonStyles = {
+        backgroundColor: '#007bff',
+        color: 'white',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        marginRight: '10px', 
+    };
+
   return (
     <>
-      <div className=" bg-slate-400  " style={customStyles}>
-            <button className=''>Add</button>
+        <div className="sticky bottom-0"  style={{ backgroundColor: 'white', padding: '10px', textAlign: 'center', borderRadius: '30px'}}>
+            <button style={buttonStyles}>Button 1</button>
+            <button style={{ ...buttonStyles, backgroundColor: '#007bff' }}>Button 2</button>
+            <button style={{ ...buttonStyles, backgroundColor: '#007bff' }}>Button 3</button>
+            <button style={{ ...buttonStyles, backgroundColor: '#007bff' }}>Button 4</button>
         </div>
     </>
   )
