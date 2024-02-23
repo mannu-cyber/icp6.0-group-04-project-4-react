e132d95b1e4a6ba991175d69f66b6b9affed14d5
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-02-23 15:14:50 +0530

    added all the components

--- src/views/UserDashboard.jsx
+++ src/views/UserDashboard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 function UserDashboard() {
   return (
-    <div>UserDashboard</div>
+    <div className='border h-screen'>
+
+
+<Footer/>
+    </div>
   )
 }
 
