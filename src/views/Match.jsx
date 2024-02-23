654958707924933def3999127bdc9ae1f6f717e7
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-02-23 19:41:29 +0530

    changes in home page

--- src/views/Match.jsx
+++ src/views/Match.jsx
@@ -4,9 +4,21 @@ import Navbar from '../components/Navbar'
 
 function Match() {
   return (
-    <div className='h-screen py-3 px-2'>
+    <div className='h-screen py-3 px-2 '>
     <Navbar/>
-     this is the match page
+
+<h1 className='text-center mt-[20px] text-[2vw] font-cursive'>Find Your Match</h1>
+
+<div className="card-container flex flex-col gap-5 w-full h-full items-center pt-9">
+
+
+
+<div className="male w-[300px] h-[200px] bg-red-500 mx-auto"></div>
+<div className="male w-[300px] h-[200px] bg-red-500 mx-auto"></div>
+
+</div>
+
+
 
     <Footer/>
     </div>
