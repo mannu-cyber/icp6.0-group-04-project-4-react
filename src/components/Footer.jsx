8a292f545dd9d21a0f56662805469f77127de51a
Abhimanyu sahani <sahanimannu844@gmail.com>
2024-02-24 11:11:47 +0530

    match page work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,19 @@ const Footer = () => {
   return (
     <footer className="fixed h-[60px]	items-center bottom-0 left-[50%] flex gap-9 w-[483px] -translate-x-[50%] justify-evenly ">
       <Link to="/home">
-        <img src="home.png" alt="" />
+        <img className="w-[25px]" src="home.png" alt="" />
       </Link>
       <Link to="/massage">
-        <img src="chat.png" alt="" />
+        <img className="w-[25px]" src="chat.png" alt="" />
       </Link>
       <Link to="/add">
-        <img src="add.png" alt="" />
+        <img className="w-[25px]" src="add.png" alt="" />
       </Link>
       <Link to="/match">
-        <img src="match.png" alt="" />
+        <img className="w-[25px]" src="match.png" alt="" />
       </Link>
       <Link to="/user">
-        <img src="user.png" alt="" />
+        <img className="w-[25px]" src="user.png" alt="" />
       </Link>
     </footer>
   );
